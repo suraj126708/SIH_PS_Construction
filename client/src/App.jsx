74ccc04e9d8ef23c6eae470d529d99117867b975
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-08-25 01:33:25 +0530

    first-backend-text-to-front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/api/example")
+      .then((response) => {
+        setData(response.data.message);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="text-5xl">Hello Team !!</h1>
+      <h1>{data}</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./Database/db.js";
 
 dotenv.config();
@@ -10,8 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get("/api/example", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
 });
 
 const PORT = process.env.PORT || 5000;
