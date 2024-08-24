10cec9e190a372c73ca291c1964f2e28fb2d78e8
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-08-25 01:23:50 +0530

    mern-stack-setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./Database/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
