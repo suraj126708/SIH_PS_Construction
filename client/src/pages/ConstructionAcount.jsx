1a9ddc5f45b4b3f00c08f17e2fd5e859c25d2084
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-09-04 15:49:02 +0530

    constructor acount -2

--- client/src/pages/ConstructionAcount.jsx
+++ client/src/pages/ConstructionAcount.jsx
@@ -3,14 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import userBg from "../Assests/images/user.png";
 import ComplaintsList from "../components/complaintList";
-import NavBar from "../components/Navbar";
+// import NavBar from "../components/Navbar";
 
 const constructorAccount = () => {
   return (
     <>
       {/* <NavBar /> */}
       <div className="bg-white w-full relative">
-        <section className="h-[90vh] flex flex-col justify-between mt-[6.5rem]">
+        <section className="h-[95vh] flex flex-col justify-between my-[6.5rem]">
           <div className="container mt-8 w-[100%] h-[45rem] bg-white rounded-lg p-5 shadow-lg">
             <div className="content flex justify-between items-center">
               <div className="left-side w-[35%] flex flex-col items-center relative">
@@ -44,29 +44,29 @@ const constructorAccount = () => {
 
               <table className=" text-lg content-table border-collapse h-[35rem] w-[50rem] mx-auto shadow-lg rounded-t-lg overflow-hidden">
                 <thead>
-                  <tr className="bg-[#26ccca] text-white text-left font-bold">
-                    <th className="py-[30px] pl-8">Project start date</th>
+                  <tr className=" bg-[#26ccca] text-white text-left font-bold">
+                    <th className="py-[30px] pl-8">Project Name</th>
                     <th className="py-[30px] pl-8 ">Project start date</th>
                     <th className="py-[30px] pl-8">Status</th>
                     <th className="py-[30px] pl-8">complaints</th>
                   </tr>
                 </thead>
-                <tbody className="bg-white">
+                <tbody className="text-xl bg-white">
                   <tr className="border-b border-[#dddddd]">
-                    <td className="py-[30px]">01/01/24</td>
-                    <td className="py-[30px]">11.00 AM</td>
+                    <td className="py-[30px]">Parli-beed highway</td>
+                    <td className="py-[30px]">10/10/24</td>
                     <td className="py-[30px] ">Completed</td>
                     <td className="py-[30px] ">20 T-shirts</td>
                   </tr>
                   <tr className="border-b border-[#dddddd] bg-[#f3f3f3]">
-                    <td className="py-[30px] ">02/02/24</td>
-                    <td className="py-[30px] ">2.00 PM</td>
+                    <td className="py-[30px] ">Parli-beed highway</td>
+                    <td className="py-[30px] ">10/10/24</td>
                     <td className="py-[30px] ">Completed</td>
                     <td className="py-[30px] ">30 Books</td>
                   </tr>
                   <tr className="border-b-2 border-[#53b5a1]">
-                    <td className="py-[30px] ">08/04/24</td>
-                    <td className="py-[30px] ">3.00 PM</td>
+                    <td className="py-[30px] ">Parli-beed highway</td>
+                    <td className="py-[30px] ">10/10/24</td>
                     <td className="py-[30px] ">Pending</td>
                     <td className="py-[30px] ">15 Bags</td>
                   </tr>
